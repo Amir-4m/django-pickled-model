df2edb1104f8aacbb4f7db5a76c38b5eb1d40387
Amir <amiri.amir30@yahoo.com>
2022-09-14 18:36:09 +0430

    removed permission for API

--- src/pickles/api/views.py
+++ src/pickles/api/views.py
@@ -1,15 +1,12 @@
 from rest_framework import views
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import Pickle
 
 
 class PicklesAPIView(views.APIView):
     """Shows init configs params"""
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         configs = Pickle.objects.all()
